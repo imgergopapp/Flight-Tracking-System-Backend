540261ecb72019c16b456ce9b25f1a2758f08963
Gergő Papp <imgergopapp@gmail.com>
2019-10-13 19:43:01 +0200

    Complete FlightRepository's integration tests for CRUD methods

--- src/test/java/com/irems/repository/FlightRepositoryIntegrationTest.java
+++ src/test/java/com/irems/repository/FlightRepositoryIntegrationTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.hibernate.type.LocalDateTimeType;
@@ -15,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.fasterxml.jackson.databind.Module.SetupContext;
 import com.irems.entity.Flight;
 
 @RunWith(SpringRunner.class)
@@ -26,21 +28,58 @@ public class FlightRepositoryIntegrationTest {
 
 	@Autowired
 	private FlightRepository flightRepository;
-
+	
+	private Flight flight = new Flight(
+			"TestCompany",
+			LocalDateTime.of(2019, 05, 05, 10, 0, 5),
+			LocalDateTime.of(2019, 05, 05, 10, 0, 5),
+			LocalDateTime.of(2019, 05, 05, 11, 0, 0)
+	);
+	
 	@Test
 	public void findById_FlightReturnedByIdOfExistingFlight() {
-		// given
-
-		Flight flight = new Flight("TestCompany", LocalDateTime.of(2019, 05, 05, 10, 0, 5),
-				LocalDateTime.of(2019, 05, 05, 10, 0, 5), LocalDateTime.of(2019, 05, 05, 10, 30, 0));
 		entityManager.persist(flight);
 		entityManager.flush();
-
-		// when
 		Optional<Flight> found = flightRepository.findById(flight.getId());
-
-		// then
 		assertEquals(flight, found.get());
 	}
 
+	@Test
+	public void save_FlightReturnedByIdOfSavedFlight() {
+		flightRepository.save(flight);
+		Optional<Flight> found = flightRepository.findById(flight.getId());
+		assertEquals(flight, found.get());
+	}
+	
+	@Test
+	public void save_UpdatedFlightSavedAndReturned() {
+		flightRepository.save(flight);
+		flight.setCompany("UpdatedCompany");
+		flightRepository.save(flight);
+		Optional<Flight> found = flightRepository.findById(flight.getId());
+		assertEquals(flight, found.get());
+	}
+	
+	@Test
+	public void deleteById_NoFlightReturnedByIdOfDeletedFlight() {
+		entityManager.persist(flight);
+		entityManager.flush();
+		flightRepository.deleteById(flight.getId());
+		Optional<Flight> found = flightRepository.findById(flight.getId());
+		assertTrue(found.isEmpty());
+	}
+
+	@Test
+	public void findAll_ReturnAllFlights() {
+		Flight anotherFlight = new Flight(
+				"AnotherCompany",
+				LocalDateTime.of(2019, 05, 05, 10, 0, 5),
+				LocalDateTime.of(2019, 05, 05, 10, 0, 5),
+				LocalDateTime.of(2019, 05, 05, 11, 0, 0)
+		);
+		flightRepository.save(anotherFlight);
+		flightRepository.save(flight);
+		List<Flight> fligths = flightRepository.findAll();
+		assertTrue(fligths.size() == 2);
+	}
 }
