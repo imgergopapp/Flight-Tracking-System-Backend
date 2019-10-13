e7c675af5c3b901f8778b40aaaf2e804c5edbd2c
Gergő Papp <imgergopapp@gmail.com>
2019-10-13 20:04:03 +0200

    Implement FlightService interface with FlightRepository

---
+++ src/main/java/com/irems/service/simple/SimpleFlightService.java
@@ -0,0 +1,37 @@
+package com.irems.service.simple;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.irems.entity.Flight;
+import com.irems.repository.FlightRepository;
+import com.irems.service.FlightService;
+
+public class SimpleFlightService implements FlightService {
+
+	@Autowired
+	FlightRepository flightRepository;
+
+	@Override
+	public Optional<Flight> findById(Integer id) {
+		return flightRepository.findById(id);
+	}
+
+	@Override
+	public void save(Flight flight) {
+		flightRepository.save(flight);
+	}
+
+	@Override
+	public List<Flight> findAll() {
+		return flightRepository.findAll();
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		flightRepository.deleteById(id);
+	}
+
+}
