b5d4589280574f0a53ffa32e27615c90b280e9fb
Gergő Papp <imgergopapp@gmail.com>
2019-10-13 19:21:56 +0200

    Extend FlightRepository interface with CRUD methods

--- src/main/java/com/irems/repository/FlightRepository.java
+++ src/main/java/com/irems/repository/FlightRepository.java
@@ -1,5 +1,6 @@
 package com.irems.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,8 +10,16 @@ import com.irems.entity.Flight;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
-	
+
 	@Override
 	public Optional<Flight> findById(Integer id);
+
+	@Override
+	public <S extends Flight> S save(S entity);
+
+	@Override
+	public List<Flight> findAll();
+
+	public void deleteById(Integer id);
 
 }
