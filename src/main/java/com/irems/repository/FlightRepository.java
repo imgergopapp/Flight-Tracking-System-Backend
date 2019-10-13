d842f235e8f337c9683d8c4248ecc0d7c19b13bb
Gergő Papp <imgergopapp@gmail.com>
2019-10-13 16:58:15 +0200

    Fix return type parameter at FlightRepository

--- src/main/java/com/irems/repository/FlightRepository.java
+++ src/main/java/com/irems/repository/FlightRepository.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.irems.entity.Flight;
+
 @Repository
-public interface FlightRepository extends JpaRepository<FlightRepository, Integer> {
+public interface FlightRepository extends JpaRepository<Flight, Integer> {
 	
 	@Override
-	public Optional<FlightRepository> findById(Integer id);
+	public Optional<Flight> findById(Integer id);
 
 }
