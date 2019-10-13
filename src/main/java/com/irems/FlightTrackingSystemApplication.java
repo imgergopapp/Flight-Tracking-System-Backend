eb9cd6e98aaa726eeb31c20ab7096f2f3cb489b9
Gergő Papp <imgergopapp@gmail.com>
2019-10-13 11:43:24 +0200

    Add Maven project base

---
+++ src/main/java/com/irems/FlightTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.irems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTrackingSystemApplication.class, args);
+	}
+
+}
