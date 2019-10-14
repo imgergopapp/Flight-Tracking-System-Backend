575db2f3e4a74de8d7a877682de9264a5a9ceb8b
Gergő Papp <imgergopapp@gmail.com>
2019-10-14 08:51:19 +0200

    Add javadoc to CorsConfiguration class

--- src/main/java/com/irems/security/CorsConfiguration.java
+++ src/main/java/com/irems/security/CorsConfiguration.java
@@ -6,9 +6,19 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * CorsConfiguration sets up Cross-Origin Resource Sharing.
+ *
+ * For more information check out {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS}.
+ */
 @Configuration
 @EnableWebMvc
 public class CorsConfiguration {
+	
+	/**
+     * corsConfigurer enables Cross-Origin Resource Sharing.
+     * @return WebMvcConfigurer
+     */
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
